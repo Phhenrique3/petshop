99dae2e30034765ea56cb4e6ecb77123d0bcb241
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-07-10 21:47:38 -0300

    CadastroCliente

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Petshop Frontend</h1>
+    <CadastroCliente />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import CadastroCliente from './components/CadastroCliente.vue'
+</script>
---
+++ src/components/CadastroCliente.vue
@@ -0,0 +1,18 @@
+<template>
+  <form @submit.prevent="CadastraCliente">
+    <h2>Caadastro do cliente</h2>
+
+    <input v-model="mome" placeholder="Nome" required />
+    <input v-model="email" placeholder="Email" type="emial" required />
+    <input
+      v-model="senha"
+      placeholder="Senha"
+      type="password"
+      required
+      minlength="6"
+    />
+
+    <button type="submit">Cadastrar</button>
+
+  </form>
+</template>
